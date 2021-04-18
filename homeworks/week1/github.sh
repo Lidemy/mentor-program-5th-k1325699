b89bc99ac0c3c0c397fa037ae84ca4f0ecc09e13
k1325699 <k1325699@gmail.com>
2021-04-18 17:40:34 +0800

    超級挑戰題更改curl -s

--- homeworks/week1/github.sh
+++ homeworks/week1/github.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl https://api.github.com/users/$1 > temp.txt
+curl -s https://api.github.com/users/$1 > temp.txt
 cat temp.txt | grep -n '"name"' | awk 'BEGIN {FS=":"} {print $3}' |sed 's/"//1' |sed 's/",//1'
 cat temp.txt | grep -n bio | awk 'BEGIN {FS=":"} {print $3}' |sed 's/"//1' |sed 's/",//1'
 cat temp.txt | grep -n location | awk 'BEGIN {FS=":"} {print $3}' |sed 's/"//1' |sed 's/",//1'
