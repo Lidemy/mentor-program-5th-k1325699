a945832c7011e588858742f0e72d66cae413ce5f
k1325699 <k1325699@gmail.com>
2021-04-25 11:09:49 +0800

    week2作業完成

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,9 @@
 function reverse(str) {
-  
+  let ans=""
+  for(i=0;i<str.length;i++){
+    ans+=str[str.length-i-1]
+  }
+  return ans
 }
 
-reverse('hello');
+console.log(reverse('hello'));
--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,10 +1,18 @@
 function join(arr, concatStr) {
-  
+  let ans=""
+  for(let i=0;i<arr.length;i++){
+    ans+=arr[i]+concatStr
+  }
+  return ans
 }
 
 function repeat(str, times) {
-  
+  let ans=""
+  for(let i=1;i<=times;i++){
+    ans+=str
+  }
+  return ans
 }
 
-console.log(join(['a'], '!'));
+console.log(join(["a"], '!'));
 console.log(repeat('a', 5));
