0a6a449ce1cee106005d9733e50eae65c5c6c0f9
k1325699 <k1325699@gmail.com>
2021-05-09 17:21:06 +0800

    hw1更改

--- homeworks/week4/hw1.js
+++ homeworks/week4/hw1.js
@@ -3,7 +3,17 @@ const request = require('request')
 request(
   'https://lidemy-book-store.herokuapp.com/books?_limit=10',
   (err, response, body) => {
-    const data = JSON.parse(body)
+    if (err) {
+      console.log('err', err)
+      return
+    }
+    let data
+    try {
+      data = JSON.parse(body)
+    } catch (err) {
+      console.log('err', err)
+      return
+    }
     for (let i = 0; i < data.length; i++) {
       console.log(data[i].id, data[i].name)
     }
