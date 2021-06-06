b92f059560c9948b5acf188c88303d6e419c688e
k1325699 <k1325699@gmail.com>
2021-06-06 23:04:56 +0800

    week8作業檢討完畢

--- homeworks/week8/hw1/js/all.js
+++ homeworks/week8/hw1/js/all.js
@@ -1,101 +1,75 @@
+const apiUrl = 'https://dvwhnbka7d.execute-api.us-east-1.amazonaws.com/default/lottery'
+const errorMessage = '系統不穩定，請再試一次'
+
+//  抽獎
+// cd function(err,data)
+function getPrize(cb) {
+  const xhr = new XMLHttpRequest()
+  xhr.addEventListener('load', (e) => {
+    if (xhr.status >= 200 && xhr.status < 400) {
+      let data
+      try {
+        data = JSON.parse(xhr.response)
+      } catch (error) {
+        cb(errorMessage)
+        return
+      }
+      if (!data.prize) {
+        cb(errorMessage)
+      }
+      cb(null, data)
+    } else {
+      cb(errorMessage)
+    }
+  })
+  xhr.open('GET', apiUrl, true)
+  xhr.send()
+}
+
 document.querySelector('.banner').addEventListener('click', (e) => {
   if (e.target.classList.contains('lottery__btn')) {
-    const request = new XMLHttpRequest()
     const banner = document.querySelector('.banner')
-    let removeClassName
-    if (banner.classList.contains('first-prize') || banner.classList.contains('second-prize') || banner.classList.contains('third-prize') || banner.classList.contains('none-prize')) {
-      removeClassName = function removeClassName(className) {
-        banner.classList.remove(className)
-      }
-      removeClassName('first-prize')
-      removeClassName('second-prize')
-      removeClassName('third-prize')
-      removeClassName('none-prize')
-      banner.innerHTML = `
-      <div class="lottery">
-        <h2>
-          2020 夏日輕盈特賞！ 抽獎活動辦法
-        </h2>
-        <div>
-          <div class="lottery__title">活動期間：</div>
-          <div class="lottery__content">2020/06/01~2020/07/01</div>
-        </div>
-        <hr>
-        <div>
-          <div class="lottery__title">活動說明：</div>
-          <div class="lottery__content">
-            今天老闆佛心來著決定給大家發獎勵，有看有機會，沒看只能幫QQ！只要在店內消費滿1000000元即有機會獲得 - 頭獎日本東京來回雙人遊！
-          </div>
-        </div>
-        <hr>
-        <div>
-          <div class="lottery__title">
-            獎&emsp;品：
-          </div>
-          <div class="lottery__content">
-            ❤ 頭獎一名：日本東京來回雙人遊(市價14990元)
-            <br>
-            ❤ 貳獎三名：90 吋電視一台(市價5990元)
-            <br>
-            ❤ 參獎十名：知名 YouTuber 簽名握手會入場券一張(市價1500元)
-          </div>
-        </div>
-        <div>
-          <button class="lottery__btn">我要抽獎</button>
-        </div>
-      </div>    
-      `
-      return
+    if (e.target.closest('.prize')) {
+      window.location.reload()
     }
-    request.addEventListener('load', (e) => {
-      const response = request.responseText
-      let json = ''
-      if (request.status >= 200 && request.status < 400) {
-        try {
-          json = JSON.parse(response)
-        } catch (error) {
-          alert('系統不穩定，請再試一次')
-        }
-        if (json.prize === 'FIRST') {
-          banner.classList.add('first-prize')
-          banner.innerHTML = `
-            <div class='prize'>
-              <h2>恭喜你中頭獎了！日本東京來回雙人遊！</h2>
-              <button class="lottery__btn">我要抽獎</button>
-            </div>
-          `
-        } else if (json.prize === 'SECOND') {
-          banner.classList.add('second-prize')
-          banner.innerHTML = `
-            <div class='prize'>
-              <h2>二獎！90 吋電視一台！</h2>
-              <button class="lottery__btn">我要抽獎</button>
-            </div>
-          `
-        } else if (json.prize === 'THIRD') {
-          banner.classList.add('third-prize')
-          banner.innerHTML = `
-            <div class='prize'>
-              <h2>恭喜你抽中三獎：知名 YouTuber 簽名握手會入場券一張，bang！</h2>
-              <button class="lottery__btn">我要抽獎</button>
-            </div>
-          `
-        } else if (json.prize === 'NONE') {
-          banner.classList.add('none-prize')
-          banner.innerHTML = `
-            <div class='prize'>
-              <h2>銘謝惠顧</h2>
-              <button class="lottery__btn">我要抽獎</button>
-            </div>
-          `
-        } else {
-          alert('系統不穩定，請再試一次')
-        }
-      } else {
-        alert('系統不穩定，請再試一次')
+    getPrize((err, data) => {
+      if (err) {
+        alert(err)
+        return
+      }
+      if (data.prize === 'FIRST') {
+        banner.classList.add('first-prize')
+        banner.innerHTML = `
+          <div class='prize'>
+            <h2>恭喜你中頭獎了！日本東京來回雙人遊！</h2>
+            <button class="lottery__btn">我要抽獎</button>
+          </div>
+        `
+      } else if (data.prize === 'SECOND') {
+        banner.classList.add('second-prize')
+        banner.innerHTML = `
+          <div class='prize'>
+            <h2>二獎！90 吋電視一台！</h2>
+            <button class="lottery__btn">我要抽獎</button>
+          </div>
+        `
+      } else if (data.prize === 'THIRD') {
+        banner.classList.add('third-prize')
+        banner.innerHTML = `
+          <div class='prize'>
+            <h2>恭喜你抽中三獎：知名 YouTuber 簽名握手會入場券一張，bang！</h2>
+            <button class="lottery__btn">我要抽獎</button>
+          </div>
+        `
+      } else if (data.prize === 'NONE') {
+        banner.classList.add('none-prize')
+        banner.innerHTML = `
+          <div class='prize'>
+            <h2>銘謝惠顧</h2>
+            <button class="lottery__btn">我要抽獎</button>
+          </div>
+        `
       }
     })
-    request.open('GET', 'https://dvwhnbka7d.execute-api.us-east-1.amazonaws.com/default/lottery', true)
-    request.send()
   }
 })
--- homeworks/week8/hw2/js/all.js
+++ homeworks/week8/hw2/js/all.js
@@ -1,91 +1,84 @@
-const request = new XMLHttpRequest()
+const apiUrl = 'https://api.twitch.tv/kraken'
+const clientID = '7vxwhu3fnd6lqfy4fg452bnwpgnixa'
 
-request.onload = (e) => {
-  if (request.status >= 200 && request.status < 400) {
-    const popularGame = JSON.parse(request.responseText).top
-    document.querySelector('.header ul').innerHTML = `
-    <li><a href="#" class="checked">${popularGame[0].game.name}</a></li>
-    <li><a href="#">${popularGame[1].game.name}</a></li>
-    <li><a href="#">${popularGame[2].game.name}</a></li>
-    <li><a href="#">${popularGame[3].game.name}</a></li>
-    <li><a href="#">${popularGame[4].game.name}</a></li>
-    `
-  }
-
-  console.log(JSON.parse(request.responseText).top[0].game.name)
-  document.querySelector('.content h2').innerText = JSON.parse(request.responseText).top[0].game.name
-  const live = new XMLHttpRequest()
-  live.onload = (e) => {
+// 串api
+// 前五名遊戲
+function getGame(callback) {
+  const request = new XMLHttpRequest()
+  request.onload = (e) => {
     if (request.status >= 200 && request.status < 400) {
-      const { streams } = JSON.parse(live.responseText).streams
-      for (let i = 0; i < streams.length; i++) {
-        const li = document.createElement('li')
-        li.innerHTML = `
-        <a href="${streams[i].channel.url}">
-        <img src="${streams[i].preview.large}" alt="" class="preview">
-          <div class="news">
-            <img src="${streams[i].channel.logo}" alt="" class="logo">
-            <div class="profiles">
-              <h3>${streams[i].channel.status}</h3>
-              <p class="channel">${streams[i].channel.name}</p>
-            </div>
-          </div>
-        </a>
-        `
-        document.querySelector('.content ul').appendChild(li)
-      }
+      const games = JSON.parse(request.responseText).top
+      callback(games)
     }
   }
-  live.open('GET', `https://api.twitch.tv/kraken/search/streams?query=${JSON.parse(request.responseText).top[0].game.name}&limit=20`, true)
-  live.setRequestHeader('Client-ID', '7vxwhu3fnd6lqfy4fg452bnwpgnixa')
-  live.setRequestHeader('Accept', 'application/vnd.twitchtv.v5+json')
-  live.send()
+  request.open('GET', `${apiUrl}/games/top?limit=5`, true)
+  request.setRequestHeader('Client-ID', clientID)
+  request.setRequestHeader('Accept', 'application/vnd.twitchtv.v5+json')
+  request.send()
 }
-request.open('GET', 'https://api.twitch.tv/kraken/games/top?limit=5', true)
-request.setRequestHeader('Client-ID', '7vxwhu3fnd6lqfy4fg452bnwpgnixa')
-request.setRequestHeader('Accept', 'application/vnd.twitchtv.v5+json')
-request.send()
+// 前20頻道
+function getStream(gameName, callback) {
+  const request = new XMLHttpRequest()
+  request.onload = (e) => {
+    if (request.status >= 200 && request.status < 400) {
+      const data = JSON.parse(request.responseText).streams
+      callback(data)
+    }
+  }
+  request.open('GET', `${apiUrl}/search/streams?query=${encodeURIComponent(gameName)}&limit=20`, true)
+  request.setRequestHeader('Client-ID', clientID)
+  request.setRequestHeader('Accept', 'application/vnd.twitchtv.v5+json')
+  request.send()
+}
 
-document.querySelector('.header').addEventListener('click', (e) => {
+// 新增前20頻道
+function appendStreams(stream) {
+  const li = document.createElement('li')
+  li.innerHTML = `
+    <a href="${stream.channel.url}">
+      <img src="${stream.preview.large}" alt="" class="preview">
+      <div class="news">
+        <img src="${stream.channel.logo}" alt="" class="logo">
+        <div class="profiles">
+          <h3>${stream.channel.status}</h3>
+          <p class="channel">${stream.channel.name}</p>
+        </div>
+      </div>
+    </a>
+    `
+  document.querySelector('.content ul').appendChild(li)
+}
+// 清單
+getGame((games) => {
+  for (const game of games) {
+    const element = document.createElement('li')
+    const gameName = game.game.name
+    element.innerHTML = `
+    <a href="#">${gameName}</a>
+    `
+    document.querySelector('.header ul').appendChild(element)
+  }
+  changeGame(games[0].game.name)
+})
+function changeGame(gameName) {
+  const checked = document.querySelectorAll('.header a')
+  for (let i = 0; i < checked.length; i++) {
+    checked[i].classList.remove('checked')
+    if (checked[i].innerText === gameName) {
+      checked[i].classList.add('checked')
+    }
+  }
+  document.querySelector('.content h2').innerText = gameName
+  document.querySelector('.content ul').innerText = ''
+  getStream(gameName, (streams) => {
+    for (const stream of streams) {
+      appendStreams(stream)
+    }
+  })
+}
+document.querySelector('.header ul').addEventListener('click', (e) => {
   if (e.target.tagName.toLowerCase() === 'a') {
     e.preventDefault()
-    const removChecked = document.querySelectorAll('.header a')
-    for (let i = 0; i < removChecked.length; i++) {
-      removChecked[i].classList.remove('checked')
-    }
-    e.target.classList.add('checked')
-    document.querySelector('.content h2').innerText = e.target.innerText
-    console.log(document.querySelector('.content ul'))
-    const removes = document.querySelectorAll('.content li')
-    for (const remove of removes) {
-      remove.remove()
-    }
-    console.log(e.target.innerText)
-    const live = new XMLHttpRequest()
-    live.onload = (e) => {
-      if (request.status >= 200 && request.status < 400) {
-        const { streams } = JSON.parse(live.responseText).streams
-        for (let i = 0; i < streams.length; i++) {
-          const li = document.createElement('li')
-          li.innerHTML = `
-          <a href="${streams[i].channel.url}">
-            <img src="${streams[i].preview.large}" alt="" class="preview">
-            <div class="news">
-              <img src="${streams[i].channel.logo}" alt="" class="logo">
-              <div class="profiles">
-                <h3>${streams[i].channel.status}</h3>
-                <p class="channel">${streams[i].channel.name}</p>
-              </div>
-            </div>
-          </a>
-          `
-          document.querySelector('.content ul').appendChild(li)
-        }
-      }
-    }
-    live.open('GET', `https://api.twitch.tv/kraken/search/streams?query=${e.target.innerText}&limit=20`, true)
-    live.setRequestHeader('Client-ID', '7vxwhu3fnd6lqfy4fg452bnwpgnixa')
-    live.setRequestHeader('Accept', 'application/vnd.twitchtv.v5+json')
-    live.send()
+    changeGame(e.target.innerText)
   }
 })
