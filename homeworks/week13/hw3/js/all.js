89f66107a3f6d60d054fbce7af247aef92eee2f0
k1325699 <k1325699@gmail.com>
2021-07-12 12:20:02 +0800

    week13 hw3 檢討

--- homeworks/week13/hw3/js/all.js
+++ homeworks/week13/hw3/js/all.js
@@ -2,34 +2,28 @@ const apiUrl = 'https://api.twitch.tv/kraken'
 const clientID = '7vxwhu3fnd6lqfy4fg452bnwpgnixa'
 // 串api
 // 前五名遊戲
-function getGame(callback) {
-  fetch(`${apiUrl}/games/top?limit=5`, {
+async function getGame() {
+  const response = await fetch(`${apiUrl}/games/top?limit=5`, {
     headers: {
       'Client-ID': clientID,
       Accept: 'application/vnd.twitchtv.v5+json'
     },
     mode: 'cors'
-  }).then((response) => response.json()
-  ).then((json) => {
-    callback(json.top)
-  }).catch((err) => {
-    console.log('err', err)
   })
+  const data = await response.json()
+  return data
 }
 // 前20頻道
-function getStream(gameName, callback) {
-  fetch(`${apiUrl}/search/streams?query=${encodeURIComponent(gameName)}&limit=20`, {
+async function getStream(gameName) {
+  const response = await fetch(`${apiUrl}/search/streams?query=${encodeURIComponent(gameName)}&limit=20`, {
     headers: {
       'Client-ID': clientID,
       Accept: 'application/vnd.twitchtv.v5+json'
     },
     mode: 'cors'
-  }).then((response) => response.json()
-  ).then((json) => {
-    callback(json.streams)
-  }).catch((err) => {
-    console.log('err', err)
   })
+  const data = await response.json()
+  return data
 }
 
 // 新增前20頻道
@@ -50,17 +44,25 @@ function appendStreams(stream) {
   document.querySelector('.content ul').appendChild(li)
 }
 // 清單
-getGame((games) => {
-  for (const game of games) {
-    const element = document.createElement('li')
-    const gameName = game.game.name
-    element.innerHTML = `
-    <a href="#">${gameName}</a>
-    `
-    document.querySelector('.header ul').appendChild(element)
+async function runGetGame() {
+  try {
+    const json = await getGame()
+    const games = json.top
+    for (const game of games) {
+      const element = document.createElement('li')
+      const gameName = game.game.name
+      element.innerHTML = `
+      <a href="#">${gameName}</a>
+      `
+      document.querySelector('.header ul').appendChild(element)
+    }
+    changeGame(games[0].game.name)
+  } catch (err) {
+    console.log('err', err)
   }
-  changeGame(games[0].game.name)
-})
+}
+runGetGame()
+
 function changeGame(gameName) {
   const checked = document.querySelectorAll('.header a')
   for (let i = 0; i < checked.length; i++) {
@@ -71,11 +73,18 @@ function changeGame(gameName) {
   }
   document.querySelector('.content h2').innerText = gameName
   document.querySelector('.content ul').innerText = ''
-  getStream(gameName, (streams) => {
-    for (const stream of streams) {
-      appendStreams(stream)
+  async function runGetStream() {
+    try {
+      const json = await getStream(gameName)
+      const { streams } = json
+      for (const stream of streams) {
+        appendStreams(stream)
+      }
+    } catch (err) {
+      console.log('err', err)
     }
-  })
+  }
+  runGetStream()
 }
 document.querySelector('.header ul').addEventListener('click', (e) => {
   if (e.target.tagName.toLowerCase() === 'a') {
